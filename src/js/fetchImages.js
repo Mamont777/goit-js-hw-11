d8ff60c0524e9c865d21cb1c4fc54fa213def8d1
Sergii Podobrii <podobri@ukr.net>
2023-03-24 19:02:48 +0200

    Done

---
+++ src/js/fetchImages.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+export { fetchImages };
+
+// Встановлення значення ключа API
+const API_KEY = '34641575-5b7af07926b499b252d30275f';
+const API_URL = 'https://pixabay.com/api/';
+
+async function fetchImages(currentQuery, pageNumber = 1) {
+  const params = {
+    key: API_KEY,
+    q: currentQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: pageNumber,
+    per_page: 40,
+  };
+
+  try {
+    const response = await axios.get(API_URL, { params });
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
